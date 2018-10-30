c6ff330afe795973c01d00007bc89edd5209433b
Zack Klodnicki <klodzack@udel.edu>
2018-10-30 10:23:48 -0400

    App actually listens for requests using express.

--- WikiBackend/index.js
+++ WikiBackend/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
+const app = express();
 
-console.log("Hello, World!");
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(3542);
+
+console.log('Listening on 3542.');
