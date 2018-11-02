9507e7f596ef47863d531146b6aaf8f04b6d1333
unknown <avak1908@gmail.com>
2018-11-02 15:38:43 -0400

    Generated components for project

--- Wikipedia-Clone/src/app/app.module.ts
+++ Wikipedia-Clone/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { ArticleTitleComponent } from './article-title/article-title.component';
+import { TableOfContentsComponent } from './table-of-contents/table-of-contents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopNavComponent,
+    SideNavComponent,
+    ArticleTitleComponent,
+    TableOfContentsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Wikipedia-Clone/src/app/article-title/article-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-title',
+  templateUrl: './article-title.component.html',
+  styleUrls: ['./article-title.component.css']
+})
+export class ArticleTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
