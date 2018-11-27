fdce80c7dcf040f3241c992ea4b83776d22bc19e
Zack Klodnicki <klodzack@udel.edu>
2018-11-27 09:58:52 -0500

    Connect to mongo

--- WikiBackend/index.js
+++ WikiBackend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express();
 const Router = require('./router.js');
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost/wiki');
+
+console.log('Connected to Mongo database.');
+
+const app = express();
 
 Router.addRoutes(app);
 
