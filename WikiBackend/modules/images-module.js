f46633e6941d91641a725ef5efa82238b178d0bd
Zack Klodnicki <klodzack@udel.edu>
2018-11-29 10:28:15 -0500

    Implemented imageModule

--- WikiBackend/modules/images-module.js
+++ WikiBackend/modules/images-module.js
@@ -1,27 +1,59 @@
+const { Image } = require('../models/image');
+
 function isValidImage(image) {
   return image && image.length > 0;
 }
 
-function postImage(res, imageData) {
-  res.set('Content-Type', 'text/plain')
-    .status(201).send('1a2b3c4d5e6f');
+function internalServerError(res) {
+  // internalServerError returns an anonymous function.
+  // The function will, using the res argument to internalServerError,
+  // send a 500 response with the anonymous argument toString.
+  return err => res.status(500).send(err.toString());
 }
 
-function getAllImages(res) {
-  res.json([
-    '1ab23c4d5e6f',
-    '6f5e4d3c2b1a'
-  ]);
+async function postImage(res, contentType, imageData) {
+  console.log(imageData);
+
+  const img = new Image();
+  img.contentType = contentType;
+  img.data = imageData;
+  await img.save()
+    .catch(internalServerError(res));
+  
+  res
+    .set('Content-Type', 'text/plain')
+    .status(201)
+    .send(img._id);
 }
 
-function getImage(res, image) {
-  if (!isValidImage(image)) return res.status(400).send();
+async function getAllImages(res) {
+  const list = await Image.find({},  {_id: true})
+    .catch(internalServerError(res));
 
-  res.sendFile(__dirname + '/testimage.png');
+  res.json(list.map(x => x._id));
 }
 
-function deleteImage(res, image) {
-  if (!isValidImage(image)) return res.status(400).send();
+async function getImage(res, imageId) {
+  if (!isValidImage(imageId)) return res.status(400).send();
+
+  const img = await Image.findOne({_id: imageId})
+    .catch(internalServerError(res));
+
+  if (img === null) return res.status(404).send();
+
+  res.contentType(img.contentType).send(img.data);
+}
+
+async function deleteImage(res, imageId) {
+  if (!isValidImage(imageId)) return res.status(400).send();
+
+  const img = await Image.findOne({_id: imageId})
+    .catch(internalServerError(res));
+
+  if (img === null) return res.status(404).send();
+
+  await img.remove()
+    .catch(internalServerError(res));
 
   res.status(204).send();
 }
