30cf7b0f33f02ee0046d2e88e5d77046cbe6e31a
Zack Klodnicki <klodzack@udel.edu>
2018-12-02 17:55:23 -0500

    Fixed compilation

--- WikiFrontEnd/src/app/components/landing/trending.pipe.ts
+++ WikiFrontEnd/src/app/components/landing/trending.pipe.ts
@@ -1,8 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import { Page } from '../../models/ud-pages';
-import { ConsoleReporter } from 'jasmine';
 
-@Pipe({name: 'trending'}) export class TrendingPipe {
+@Pipe({name: 'trending'}) export class TrendingPipe implements PipeTransform {
   transform(
     values: Page[],
   ) {
