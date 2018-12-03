7e42b2fce071b5ee053613762a88e93eb528bc3d
Zack Klodnicki <klodzack@udel.edu>
2018-12-02 22:37:16 -0500

    Display images from DB or outside world

--- WikiFrontEnd/src/app/markdown.settings.ts
+++ WikiFrontEnd/src/app/markdown.settings.ts
@@ -1,14 +1,23 @@
 import { MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 
+const outsideRegex = /^https?:\/\//;
+
 const renderer = new MarkedRenderer();
 
 renderer.link = (href, title, text) => {
-  const outsideRegex = /^https?:\/\//;
   if (outsideRegex.test(href)) {
     return `<a href='${href}' title='${title}' target='_blank'>${text}</a>`;
   } else {
     return `<a class='convert-link' href='#' data-href='${href}' title='${title || ''}'>${text}</a>`;
   }
+};
+
+renderer.image = (href, title, text) => {
+  let url = href;
+  if (!outsideRegex.test(href)) {
+    url = `http://localhost:3542/api/images/${href}`;
+  }
+  return `<img src='${url}' alt='${text}' title='${title || ''}' />`;
 };
 
 export const MARKDOWN_SETTINGS: MarkedOptions = {
