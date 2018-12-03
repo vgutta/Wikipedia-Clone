7664fe2ab200c24dc9c6996721f8e2bd52c4c550
vgutta <vineethg@udel.edu>
2018-12-03 12:13:08 -0500

    fixed conflicts

--- WikiFrontEnd/src/app/app-routing.module.ts
+++ WikiFrontEnd/src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
-<<<<<<< HEAD
 import { ViewEditPageComponent } from './components/view-edit-page/view-edit-page.component';
 import { RetrievePageResolve } from './guards/retrieve-page-resolver.service';
-=======
 import { CallbackComponent } from './callback/callback.component';
->>>>>>> security
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'landing'},
   {path: 'landing', component: LandingComponent},
-<<<<<<< HEAD
-  {path: 'wiki/:pageName', component: ViewEditPageComponent, resolve: { page: RetrievePageResolve } }
-=======
+  {path: 'wiki/:pageName', component: ViewEditPageComponent, resolve: { page: RetrievePageResolve } },
   {path: 'landing/error', component: LandingComponent},
   {path: 'callback', component: CallbackComponent }
->>>>>>> security
 ];
 
 @NgModule({
