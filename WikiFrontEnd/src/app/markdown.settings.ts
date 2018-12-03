9b8eec82f05f64decab7a3da70a5f5375dcd465d
Zack Klodnicki <klodzack@udel.edu>
2018-12-02 22:00:11 -0500

    Outside links now work too

--- WikiFrontEnd/src/app/markdown.settings.ts
+++ WikiFrontEnd/src/app/markdown.settings.ts
@@ -3,7 +3,12 @@ import { MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 const renderer = new MarkedRenderer();
 
 renderer.link = (href, title, text) => {
-  return `<a class='convert-link' href='#' data-href='${href}' title='${title || ''}'>${text}</a>`;
+  const outsideRegex = /^https?:\/\//;
+  if (outsideRegex.test(href)) {
+    return `<a href='${href}' title='${title}' target='_blank'>${text}</a>`;
+  } else {
+    return `<a class='convert-link' href='#' data-href='${href}' title='${title || ''}'>${text}</a>`;
+  }
 };
 
 export const MARKDOWN_SETTINGS: MarkedOptions = {
