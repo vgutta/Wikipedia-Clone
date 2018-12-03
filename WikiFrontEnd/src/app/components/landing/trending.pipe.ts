e4bb4384818c4816c685fe26ee7acd8b3d686591
vgutta <vineethg@udel.edu>
2018-12-02 21:48:28 -0500

    landing.component.html

--- WikiFrontEnd/src/app/components/landing/trending.pipe.ts
+++ WikiFrontEnd/src/app/components/landing/trending.pipe.ts
@@ -1,6 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import { Page } from '../../models/ud-pages';
-import { ConsoleReporter } from 'jasmine';
+//import { ConsoleReporter } from 'jasmine';
 
 @Pipe({name: 'trending'}) export class TrendingPipe {
   transform(
@@ -9,11 +9,9 @@ import { ConsoleReporter } from 'jasmine';
     let pagevisits = Object.assign([], values);
 
     pagevisits.sort((a, b) => {
-      //console.log(a);
       return b.pagevisits - a.pagevisits;  
 
     });
-    console.log(pagevisits);
     return pagevisits;
 
    
