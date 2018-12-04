eebd0c07f8edb36b13841b37fe8f362cd36996b2
d-heck <dheck@udel.edu>
2018-12-04 10:48:47 -0500

    Page routes are now restricted if not signed in.

--- WikiFrontEnd/src/app/app-routing.module.ts
+++ WikiFrontEnd/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LandingComponent } from './components/landing/landing.component';
 import { ViewEditPageComponent } from './components/view-edit-page/view-edit-page.component';
 import { RetrievePageResolve } from './guards/retrieve-page-resolver.service';
 import { CallbackComponent } from './callback/callback.component';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'landing'},
+  {path: '', pathMatch: 'full', redirectTo: 'landing',},
   {path: 'landing', component: LandingComponent},
-  {path: 'wiki/:pageName', component: ViewEditPageComponent, resolve: { page: RetrievePageResolve } },
+  {path: 'wiki/:pageName', component: ViewEditPageComponent, resolve: { page: RetrievePageResolve }, canActivate: [AuthGuard] },
   {path: 'landing/error', component: LandingComponent},
   {path: 'callback', component: CallbackComponent }
 ];
